f3562c3657c638f2c8b74eab440156882cc15f47
drldx <168914904+drldx@users.noreply.github.com>
2025-10-07 14:09:35 +0530

    feat: change grid size
    a button to get user specified size of grid and update the ui with given
    size of grids

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const container = document.querySelector(".container");
+const gridBtn = document.querySelector("button");
+const root = document.documentElement;
 
-for(let i = 0; i < 16; i++){
-  
-  for(let j = 0; j < 16; j++){
+function renderGrid(size){
+
+  for(let i = 0; i < size * size; i++){
 
     const divs = document.createElement("div");
     divs.classList.add("square");
     container.appendChild(divs);
 
+    divs.addEventListener("mouseover", (e)=>{
+     e.target.style.background = "white"; 
+    });
   }
 }
+
+renderGrid(16);
+
+function delGrid(){
+  container.textContent = ''; 
+}
+
+function changeGrid(){
+  const size = prompt("Grid Size: ", "");
+  if (!size || size > 100) {
+   alert("Range 1 to 100");
+   return; 
+  }
+  root.style.setProperty('--count', size);  
+  delGrid();
+  renderGrid(size);
+}
+
+gridBtn.addEventListener("click", changeGrid);
