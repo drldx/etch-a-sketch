3391c0579b16edf783ba6f73f4ed2399592baf35
drldx <168914904+drldx@users.noreply.github.com>
2025-10-07 12:39:05 +0530

    add: basic skeleton of the page
    setup an html file with styles.css and script.js linked to form a basic working
    template;

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container");
+
+for(let i = 0; i <= 16; i++){
+  
+  for(let j = 0; j <= 16; j++){
+
+    const divs = document.createElement("div");
+    divs.classList.add("square");
+    container.appendChild(divs);
+
+  }
+}
