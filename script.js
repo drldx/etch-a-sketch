3d2e0f1b662e54140e209ac52ae4eba3dfc4cccd
drldx <168914904+drldx@users.noreply.github.com>
2025-10-07 13:05:37 +0530

    add: grid like divs to the container
    using Dom and classList property, a 16x16 grid implemented on the page

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i <= 16; i++){
+for(let i = 0; i < 16; i++){
   
-  for(let j = 0; j <= 16; j++){
+  for(let j = 0; j < 16; j++){
 
     const divs = document.createElement("div");
     divs.classList.add("square");
